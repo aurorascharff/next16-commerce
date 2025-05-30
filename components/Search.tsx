4797cbec5b147b7eaeca5b71c9ea9f6bddb8ad58
Aurora Scharff <aurora.sofie@gmail.com>
2025-05-30 11:14:29 +0200

    Update search

--- components/Search.tsx
+++ components/Search.tsx
@@ -2,13 +2,14 @@
 
 import Form from 'next/form';
 import { useRouter, useSearchParams } from 'next/navigation';
-import React from 'react';
+import React, { useTransition } from 'react';
 import SearchStatus from './ui/SearchStatus';
 
 export default function Search() {
   const searchParams = useSearchParams();
   const q = searchParams.get('q') || '';
   const router = useRouter();
+  const [isPending, startTransition] = useTransition();
 
   return (
     <>
@@ -17,16 +18,16 @@ export default function Search() {
           className="border-divider placeholder:text-gray focus:border-primary focus:outline-primary dark:border-divider-dark dark:bg-card-dark w-full rounded-md border bg-white px-4 py-2 pl-9 text-sm outline-offset-2 focus:outline-2"
           name="q"
           onChange={e => {
-            if (e.target.value === '') {
-              router.push('?q=');
-            }
+            startTransition(() => {
+              router.push(`?q=${e.target.value}`);
+            });
           }}
           defaultValue={q}
           type="search"
           placeholder={'Search products...'}
         />
         <div className="text-gray absolute top-1/2 left-3 -translate-y-1/2">
-          <SearchStatus />
+          <SearchStatus searching={isPending} />
         </div>
       </Form>
     </>
