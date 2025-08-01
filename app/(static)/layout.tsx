00e55b5fcb78c6f0f3ac95705d2dccd6c3fcb1ca
Aurora Scharff <aurora.sofie@gmail.com>
2025-08-01 17:04:27 +0200

    Improve structure of applayout

--- app/(static)/layout.tsx
+++ app/(static)/layout.tsx
@@ -1,24 +1,6 @@
-import { ArrowLeft } from 'lucide-react';
-import Link from 'next/link';
 import React from 'react';
 import AppLayout from '@/components/layout/AppLayout';
-import LinkStatus from '@/components/ui/LinkStatus';
 
 export default async function StaticLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <AppLayout
-      headerContent={
-        <Link
-          href="/"
-          className="text-primary hover:text-primary-dark inline-flex items-center gap-2 text-sm font-medium"
-        >
-          <LinkStatus>
-            <ArrowLeft aria-hidden className="size-4" />
-          </LinkStatus>
-        </Link>
-      }
-    >
-      {children}
-    </AppLayout>
-  );
+  return <AppLayout>{children}</AppLayout>;
 }
--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,21 +1,9 @@
-import { ArrowLeft } from 'lucide-react';
-import Link from 'next/link';
 import React from 'react';
 import AppLayout from '@/components/layout/AppLayout';
 
 export default function NotFound() {
   return (
-    <AppLayout
-      headerContent={
-        <Link
-          href="/"
-          className="text-primary hover:text-primary-dark inline-flex items-center gap-2 text-sm font-medium"
-        >
-          <ArrowLeft aria-hidden className="size-4" />
-          Back Home
-        </Link>
-      }
-    >
+    <AppLayout>
       <div className="flex flex-col items-center justify-center rounded-lg text-center">
         <h1 className="text-primary text-6xl font-bold">404</h1>
         <p className="mt-4 text-xl font-semibold">Page Not Found</p>
