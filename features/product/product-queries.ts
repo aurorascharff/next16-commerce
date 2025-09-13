42bffe12f61d1c41062e77aeeb7d77f02b9f7e75
Aurora Scharff <aurora.sofie@gmail.com>
2025-09-13 15:57:52 +0200

    Add use cache

--- features/product/product-queries.ts
+++ features/product/product-queries.ts
@@ -1,5 +1,6 @@
 import 'server-only';
 
+import { cacheLife } from 'next/dist/server/use-cache/cache-life';
 import { notFound } from 'next/navigation';
 import { cache } from 'react';
 import { prisma } from '@/db';
@@ -7,8 +8,8 @@ import { slow } from '@/utils/slow';
 import { verifyAuth } from '../auth/auth-actions';
 
 export const getProduct = cache(async (productId: number) => {
-  // 'use cache';
-  // cacheLife('days');
+  'use cache';
+  cacheLife('days');
 
   await slow();
 
@@ -22,8 +23,8 @@ export const getProduct = cache(async (productId: number) => {
 });
 
 export const getProductDetails = cache(async (productId: number) => {
-  // 'use cache';
-  // cacheLife('days');
+  'use cache';
+  cacheLife('days');
 
   await slow();
 
@@ -84,8 +85,8 @@ export const getReviews = cache(async (productId: number) => {
 });
 
 export const getCategories = cache(async () => {
-  // 'use cache';
-  // cacheLife('days');
+  'use cache';
+  cacheLife('days');
 
   await slow();
 
