f131acfb2d4ae96b0ccc3a01a4547d4b9c9e01e2
Aurora Scharff <aurora.sofie@gmail.com>
2025-09-14 22:13:28 +0200

    Remove loading state

--- app/[requestContext]/layout.tsx
+++ app/[requestContext]/layout.tsx
@@ -2,7 +2,7 @@ import React, { Suspense } from 'react';
 import Header from '@/components/Header';
 import Boundary from '@/components/internal/Boundary';
 import { AuthProvider } from '@/features/auth/components/AuthProvider';
-import { ProductListSkeleton } from '@/features/product/components/ProductList';
+
 import UserProfile, { UserProfileSkeleton } from '@/features/user/components/UserProfile';
 import { getRequestContext } from '@/utils/request-context';
 
@@ -25,24 +25,9 @@ export default async function RequestContextLayout({ children, modal, params }: 
         />
       </Boundary>
       <main className="mb-4 flex flex-1 flex-col gap-4 p-4 sm:mb-8 sm:gap-10 sm:p-10 lg:mb-10 2xl:px-60">
-        <Suspense fallback={<AuthPageSkeleton />}>
-          {children}
-          {modal}
-        </Suspense>
+        {children}
+        {modal}
       </main>
     </AuthProvider>
-  );
-}
-
-function AuthPageSkeleton() {
-  return (
-    <>
-      <div className="skeleton-animation mb-4 h-30 w-full rounded-lg" />
-      <div className="skeleton-animation mb-4 h-10 w-full rounded-lg" />
-      <div className="flex h-full grow flex-col gap-4">
-        <div className="skeleton-animation h-6 w-24 rounded" />
-        <ProductListSkeleton />
-      </div>
-    </>
   );
 }
