435f990bfff1d6d623d1fb6e9f30f79074f09f14
Aurora Scharff <aurora.sofie@gmail.com>
2025-09-14 19:03:37 +0200

    Try fix cache

--- app/(auth)/@modal/(.)product/[id]/page.tsx
+++ app/(auth)/@modal/(.)product/[id]/page.tsx
@@ -1,10 +1,17 @@
+import { cacheLife } from 'next/dist/server/use-cache/cache-life';
+import { cacheTag } from 'next/dist/server/use-cache/cache-tag';
 import React, { Suspense } from 'react';
 import Modal from '@/components/ui/Modal';
 import Product, { ProductSkeleton } from '@/features/product/components/Product';
 
 export default async function ProductModal({ params }: PageProps<'/product/[id]'>) {
+  'use cache';
+
   const { id } = await params;
   const productId = Number(id);
+
+  cacheLife('days');
+  cacheTag('product-' + productId);
 
   return (
     <Modal goBackOnClose openModal={true} title="Quick Preview">
