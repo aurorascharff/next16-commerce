fb8ba2cbb5955e5c8c8788150a07c83bcd1f0635
Aurora Scharff <aurora.sofie@gmail.com>
2025-09-14 18:32:09 +0200

    Try cache components

--- next.config.ts
+++ next.config.ts
@@ -3,13 +3,13 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   experimental: {
     authInterrupts: true,
+    cacheComponents: true,
     inlineCss: true,
     ppr: true,
     reactCompiler: true,
     staleTimes: {
       dynamic: 30,
     },
-    useCache: true,
   },
   typedRoutes: true,
 };
