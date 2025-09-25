ee1fc360101adf81304318740ec511b3c55c1202
Aurora Scharff <aurora.sofie@gmail.com>
2025-09-25 17:12:26 +0200

    fix build

--- app/[requestContext]/@modal/(.)product-modal/[id]/page.tsx
+++ app/[requestContext]/@modal/(.)product-modal/[id]/page.tsx
@@ -1,12 +1,6 @@
-<<<<<<<< HEAD:app/[requestContext]/@modal/(.)product/[id]/layout.tsx
-import Modal from '@/components/ui/Modal';
-
-export default async function ProductModalLayout({ params, children }: LayoutProps<'/[requestContext]/product/[id]'>) {
-========
 import Product from '@/features/product/components/Product';
 
-export default async function ProductModal({ params }: PageProps<'/product/[id]'>) {
->>>>>>>> 0e2b4d41185eb58973b7e1e03f2778d1c200232e:app/@modal/(.)product-modal/[id]/page.tsx
+export default async function ProductModal({ params }: PageProps<'/[requestContext]/product/[id]'>) {
   const { id } = await params;
   const productId = Number(id);
 
