ac882b86fb86489bc3c3ed95e7d9fafd97470124
Aurora Scharff <aurora.sofie@gmail.com>
2025-10-19 17:54:08 +0200

    Update product page

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -1,7 +1,7 @@
 import { Suspense } from 'react';
 import BackButton from '@/components/ui/BackButton';
 import Card from '@/components/ui/Card';
-import Product from '@/features/product/components/Product';
+import Product, { ProductSkeleton } from '@/features/product/components/Product';
 import ProductDetails, { ProductDetailsSkeleton } from '@/features/product/components/ProductDetails';
 import Reviews, { ReviewsSkeleton } from '@/features/product/components/Reviews';
 
@@ -18,14 +18,23 @@ export default async function ProductPage({ params }: PageProps<'/product/[id]'>
       <BackButton />
       <div className="flex w-full flex-col gap-8 self-center md:w-[700px]">
         <Card>
-          <Product
-            productId={productId}
-            details={
-              <Suspense key={productId} fallback={<ProductDetailsSkeleton />}>
-                <ProductDetails productId={productId} />
-              </Suspense>
+          <Suspense
+            fallback={
+              <>
+                <ProductSkeleton isDetails />
+                <ProductDetailsSkeleton />
+              </>
             }
-          />
+          >
+            <Product
+              productId={productId}
+              details={
+                <Suspense key={productId} fallback={<ProductDetailsSkeleton />}>
+                  <ProductDetails productId={productId} />
+                </Suspense>
+              }
+            />
+          </Suspense>
         </Card>
         <div>
           <h2 className="mb-4 text-xl font-semibold">Customer Reviews</h2>
